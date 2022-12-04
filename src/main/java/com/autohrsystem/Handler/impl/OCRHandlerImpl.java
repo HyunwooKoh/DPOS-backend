c62e1201cce1b76dd4929efe7b3d9021a4ca9e96
hyunwoo.koh <trghyunwoo@gmail.com>
2022-12-04 20:53:43 +0900

    feat: Add override Tag

--- src/main/java/com/autohrsystem/Handler/impl/OCRHandlerImpl.java
+++ src/main/java/com/autohrsystem/Handler/impl/OCRHandlerImpl.java
@@ -24,16 +24,19 @@ public class OCRHandlerImpl implements OCRHandler {
         // TODO: do request using file
     }
 
+    @Override
     public boolean checkStatus(String taskID) {
         // TODO: check how does the task working
         // return: if finished ? true : false;
         return false;
     }
 
+    @Override
     public void getResultFile(String resFilePath) {
         // TODO: getResult json file from OCR server
     }
 
+    @Override
     public Map<String,String> getTargetData(String resFilePath) {
         // TODO: parseData and get target data
         Map<String, String> datas = new HashMap<String, String>();
@@ -41,10 +44,12 @@ public class OCRHandlerImpl implements OCRHandler {
         return datas;
     }
 
+    @Override
     public void insertToDB(Map<String,String> data) {
         // TODO: insert data to DB
     }
 
+    @Override
     public String generateKey() {
         // TODO : generate new key for new data
         return "";
