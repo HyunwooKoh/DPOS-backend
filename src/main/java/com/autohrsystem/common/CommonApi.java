aa0cde2658e483a4ee247ce585b53e8ef1ad1c7e
hyunwoo.koh <trghyunwoo@gmail.com>
2023-01-03 00:09:37 +0900

    refact: common api package 변경

--- src/main/java/com/autohrsystem/common/CommonApi.java
+++ src/main/java/com/autohrsystem/common/CommonApi.java
@@ -1,9 +1,13 @@
-package com.autohrsystem.common.api;
+package com.autohrsystem.common;
 
 import java.util.UUID;
 
 public class CommonApi {
     public static String generateTempPath() {
         return UUID.randomUUID().toString();
+    }
+
+    public static String getTempDir() {
+        return "";
     }
 }
