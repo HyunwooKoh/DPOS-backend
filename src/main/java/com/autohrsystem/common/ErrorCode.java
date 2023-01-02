97bdd03c7b35444f96ba265c44ee399fedfc9897
hyunwoo.koh <trghyunwoo@gmail.com>
2023-01-03 00:09:50 +0900

    feat: error 관련 class init

---
+++ src/main/java/com/autohrsystem/common/ErrorCode.java
@@ -0,0 +1,11 @@
+package com.autohrsystem.common;
+
+public class ErrorCode {
+
+    // File Server Error
+    public static int FSV_FILE_NOT_FOUND = -100;
+    public static int FSV_FILE_CHECK_ERROR = -101;
+    public static int FSV_FILE_ALREADY_EXIST = -102;
+    public static int FSV_FILE_DOWNLOAD_FAILED = -103;
+
+}
