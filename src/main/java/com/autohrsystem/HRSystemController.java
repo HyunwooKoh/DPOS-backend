1829f48c36b9ff2483776c57680709ed5c7c24d1
hyunwoo.koh <trghyunwoo@gmail.com>
2023-03-17 15:28:38 +0900

    chore: 멀티파트 파일 오류 분석용 커밋

--- src/main/java/com/autohrsystem/HRSystemController.java
+++ src/main/java/com/autohrsystem/HRSystemController.java
@@ -2,27 +2,33 @@ package com.autohrsystem;
 
 import com.autohrsystem.common.CommonApi;
 
+import com.autohrsystem.common.ExtractBody;
 import com.autohrsystem.executer.OCRTaskExecutorService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 public class HRSystemController {
     private final OCRTaskExecutorService m_taskExecutor = new OCRTaskExecutorService();
     protected final Logger logger = LoggerFactory.getLogger(getClass());
 
-    @PostMapping("/job/extract")
-    public void extract(@RequestBody Map<String, Object> param) {
-        String uuid = (String)param.get("uuid");
-        String reqType = (String)param.get("Type");
-        String ext = (String)param.get("ext");
+    @PostMapping(value = "/job/extract", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
+    public void extract(@ModelAttribute ExtractBody dto) {
+        System.out.println(dto);
+        String uuid = CommonApi.generateUuid();
+        String reqType = dto.getReqType();
+        String ext = dto.getExt();
+            dto.getFiles()
+                    .map(MultipartFile::getName)
+                    .subscribe(System.out::println);
+//                    .transferTo(new File(CommonApi.getTempDir(uuid) + "origin" + "ext"));
 
         m_taskExecutor.addTask(uuid, ext, reqType);
     }
