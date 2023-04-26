028e0be89de659c038dc9a7c4e3fe9ca0670496b
hyunwoo.koh <trghyunwoo@gmail.com>
2023-04-26 19:41:37 +0900

    feat: prsinfo, resume 데이터 조회 API 추가

--- src/main/java/com/autohrsystem/controller/DataController.java
+++ src/main/java/com/autohrsystem/controller/DataController.java
@@ -1,10 +1,10 @@
 package com.autohrsystem.controller;
 
 import com.autohrsystem.db.RepoManager;
-import com.autohrsystem.db.documnet.Issue.IssueEntity;
-import com.autohrsystem.db.task.TaskRepository;
+import com.autohrsystem.db.documnet.PrsInfo.PrsInfoEntity;
+import com.autohrsystem.db.documnet.Resume.ResumeEntity;
+
 import io.vertx.core.json.JsonArray;
-import io.vertx.core.json.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +18,17 @@ public class DataController {
     @Autowired
     RepoManager repoManager;
 
-    @GetMapping("/issue/all")
+    @GetMapping("/prsInfo/all")
     public String getAllIssueRecords() {
-        List<IssueEntity> entityList = repoManager.getAllIssueEntity();
+        List<PrsInfoEntity> entityList = repoManager.getAllPrsInfoEntity();
         JsonArray array = new JsonArray(entityList);
         return array.toString();
     }
 
-
+    @GetMapping("/resume/all")
+    public String getAllResumeRecords() {
+        List<ResumeEntity> entityList = repoManager.getAllResumeEntity();
+        JsonArray array = new JsonArray(entityList);
+        return array.toString();
+    }
 }
