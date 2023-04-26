08293a0200a42d6b5ce91c71cee5451a63be3bd3
hyunwoo.koh <trghyunwoo@gmail.com>
2023-04-26 19:41:17 +0900

    Del: Issue Entity 제거

--- src/main/java/com/autohrsystem/db/documnet/Issue/IssueRepository.java
+++
@@ -1,7 +0,0 @@
-package com.autohrsystem.db.documnet.Issue;
-
-import com.autohrsystem.db.documnet.Issue.IssueEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IssueRepository extends JpaRepository<IssueEntity, String> {
-}
